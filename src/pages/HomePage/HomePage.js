5d5e1690764458a47e077aa47c5cd9db61dda446
ryan-1337 <ryanaouiche@gmail.com>
2022-09-08 17:20:12 +0200

    InfoSection create

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Carrousel from "../../components/Carrousel/Caroussel";
+import InfoSection from "../../components/InfoSection/InfoSection";
 import NavBar from "../../components/NavBar/NavBar"
 
 export function HomePage() {
@@ -9,6 +10,9 @@ export function HomePage() {
         <NavBar />
         <Carrousel />
       </header>
+      <section id="info-section" class="m-5">
+        <InfoSection />
+      </section>
       <Outlet />
     </div>
   );
