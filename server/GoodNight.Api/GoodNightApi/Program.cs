26c79793980e3606ae2cb047bed1dbb3e6738d43
ryan-1337 <56730269+ryan-1337@users.noreply.github.com>
2022-09-23 11:29:29 +0200

    [Back] - Création d'un projet C# API - #105 (#5)
    * change gitignore
    
    * feat 105
    
    * feat 105 fix empty line

--- server/GoodNight.Api/GoodNightApi/Program.cs
+++ server/GoodNight.Api/GoodNightApi/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddHealthChecks().AddSqlServer(builder.Configuration.GetConnectionString("GoodNightDatabase"));
 builder.Services.AddDbContext<HotelContext>(opts =>
 {
     var connString = builder.Configuration.GetConnectionString("GoodNightDatabase");
